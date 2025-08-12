db73264d965fba070245da8d5a24ba8783f54581
Blas Antunez <blasantunez310@gmail.com>
2025-08-12 12:35:43 -0400

    Modified Database class to use User class. Added delete_user and list_users methods

--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
-import sqlite3, os
+import sqlite3
+import os
+from src.user import User
 
 
 class Database:
@@ -11,14 +13,14 @@ class Database:
         conn.execute("PRAGMA foreign_keys = ON;")
         return conn
 
-    def init_db(self, db_name: str = "passwords.db"):
+    def init_db(self) -> None:
         with self._connect() as conn:
             conn.execute(
                 """
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     email TEXT NOT NULL UNIQUE,
-                    vault_key TEXT NOT NULL
+                    vault_key _ hash TEXT NOT NULL
                 );
                 """
             )
@@ -29,21 +31,36 @@ class Database:
             os.remove(self.db_name)
         return None
 
-    def add_user(self, email: str, vault_key: str) -> int:
+    def add_user(self, user: User) -> int:
         with self._connect() as conn:
             cur = conn.execute(
                 """
-            INSERT INTO users (email, vault_key) VALUES (?, ?)
+            INSERT INTO users (email, vault_key_hash) VALUES (?, ?)
             """,
-                (email, vault_key),
+                (user.email, user.vault_key_hash),
             )
-            conn.commit()
-            return cur.lastrowid
+            user.id = cur.lastrowid
+            return user.id
 
-    def get_user(self, email: str):
+    def get_user(self, email: str) -> User:
         with self._connect() as conn:
-            cur = conn.execute(
-                "SELECT * FROM users WHERE email = ?",
-                (email,),
+            row = conn.execute(
+                "SELECT * FROM users WHERE email = ?", (email,)
+            ).fetchone()
+            if not row:
+                return None
+            return User(
+                email=row["email"], vault_key_hash=row["valut_key_hash"], id=row["id"]
             )
-            return cur.fetchone()
+
+    def delete_user(self, email: str) -> int:
+        with self._connect() as conn:
+            cur = conn.execute("DELETE FROM users WHERE email = ?", (email,))
+            return cur.rowcount
+
+    def list_users(self) -> list[User]:
+        with self.connect() as conn:
+            rows = conn.execute("SELECT * FROM users ORDER BY id").fethcall()
+            return [
+                User(row["email"], row["vault_key_hash"], row["id"]) for row in rows
+            ]
